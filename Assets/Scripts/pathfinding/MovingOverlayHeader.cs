21e7dba66ddf2e494ec83acd3f722599bdfefc6a
juliartc <83762222+juliartc@users.noreply.github.com>
2021-07-18 07:44:01 +0200

    New Moving Headers, Settings Layout

---
+++ Assets/Scripts/pathfinding/MovingOverlayHeader.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+
+public class MovingOverlayHeader : MonoBehaviour
+
+{
+
+    private float startY;
+    private int tick;
+    // Start is called before the first frame update
+    void Start()
+    {
+        this.startY = this.transform.position.y;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        this.tick++;
+        float y = (float)Math.Sin(this.tick * 0.02) * 3;
+        Vector3 newPos = new Vector3(this.transform.position.x, this.startY + y, this.transform.position.z);
+        this.transform.position = newPos;
+
+    }
+}
